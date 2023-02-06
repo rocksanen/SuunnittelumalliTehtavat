485ec0d8e2b66763b76a798fa2e36739e39d05d2
rocksanen <rocksanen@gmail.com>
2023-02-06 16:28:45 +0200

    tehtävä 5 tehty

---
+++ src/Singleton/Main.java
@@ -0,0 +1,17 @@
+package Singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user = User.getInstance();
+        user.signUp("kalle nallersson", "yykaakoonee");
+
+        System.out.println(user);
+
+        user.login("kalle nallersson","yykaakoonee");
+        user.login("ville vääränimi","kanada");
+
+
+    }
+}
---
+++ src/Singleton/User.java
@@ -0,0 +1,42 @@
+package Singleton;
+
+public class User {
+    private static User instance;
+    private String userName;
+    private String password;
+    private int userID;
+
+    public static User getInstance() {
+
+        if(instance == null) {
+
+            instance = new User();
+        }
+        return instance;
+    }
+
+    public void signUp(String name,String password) {
+
+        this.userName = name;
+        this.password = password;
+        this.userID = (int) Math.round(Math.random() * 500);
+
+    }
+
+    public void login(String name, String password) {
+
+        if(name.equals(this.userName) && password.equals(this.password)) {
+
+            System.out.println("Tervetuloa takaisin " + this.userName);
+
+        }else{
+
+            System.out.println("Ei mitään asiaa tänne!");
+        }
+    }
+
+    public String toString() {
+
+        return userName + ", " + password + ", " + userID;
+    }
+}
